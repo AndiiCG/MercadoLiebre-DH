5163026d5bdcdd8dbf0e8de1bb6d7f8e504b070e
AndiiCG <andiicg12@gmail.com>
2023-11-07 14:18:10 -0300

    Se agrega el proyecto ML

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+//configuracion de la carpeta estatica
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'views','home.html'))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname,'views','login.html'))
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname,'views','register.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`)
+})
